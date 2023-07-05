fe7f278623d284911b6d3c1ddc3b4aa39d72fabe
Ignacio Rviera <riveign@gmail.com>
2023-07-05 12:31:15 +0200

    first mutations to create tracks

--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -1,15 +1,47 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
- 
+import type { GraphQLContext } from './context'
+import type { Track } from '@prisma/client'
+
 const typeDefinitions = /* GraphQL */ `
   type Query {
-    hello: String!
+    listTracks: [Track!]!
+  }
+
+  type Track {
+    id: ID!
+    title: String!
+  }
+
+  type Mutation {
+    createTrack(title: String!): Track!
   }
 `
- 
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
-  }
+    listTracks: async (parent: unknown, args: unknown, context: GraphQLContext) => {
+      return await context.prisma.track.findMany()
+    }
+  },
+  Track: {
+    id: (parent: Track) => parent.id,
+    title: (parent: Track) => parent.title
+  },
+
+  Mutation: {
+    async createTrack(
+      parent: unknown, 
+      args: { title: string },
+      context: GraphQLContext
+      ) {
+        const newTrack = await context.prisma.track.create({
+          data: {
+            title: args.title
+          }
+        })
+        return newTrack
+    }
+  } 
 }
  
 export const schema = makeExecutableSchema({
